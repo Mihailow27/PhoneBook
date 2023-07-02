be9b2377ce9a055760890b80bbe9bf4ca0052c3a
Dobromir Mihaylov <dobromirmihalow@icloud.com>
2023-07-02 14:27:18 +0300

    SOFTUNI Project

---
+++ phonebook.js
@@ -0,0 +1,11 @@
+var phonebook = [];
+function getPhonebook() {
+  return phonebook;
+}
+function addContact(contact) {
+  phonebook.push(contact);
+}
+module.exports = {
+  getPhonebook,
+  addContact,
+};
